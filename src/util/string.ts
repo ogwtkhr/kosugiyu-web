0d5d9b72e76265773d3652d994ee806f15c92264
ogwtkhr <color_me_pop@live.jp>
2021-01-29 02:16:14 +0900

    コメント

--- src/util/string.ts
+++ src/util/string.ts
@@ -3,4 +3,5 @@ export const isEmptyString = (str: string): boolean => str === '';
 // https://qiita.com/miiitaka/items/793555b4ccb0259a4cb8
 export const stripTag = (str: string): string => str.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
 
+// https://qiita.com/str32/items/a692073af32757618042
 export const isUrl = (str: string): boolean => /https?:\/\/[\w!?/+\-_~=;.,*&@#$%()'[\]]+/.test(str);
