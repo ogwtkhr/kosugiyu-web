cc1e21875ecac6d2ee753429690d61029dce8398
ogwtkhr <color_me_pop@live.jp>
2021-02-03 14:20:56 +0900

    イラスト追加

--- src/modules/IntroModule.tsx
+++ src/modules/IntroModule.tsx
@@ -32,7 +32,12 @@ export const IntroModule: React.FC = () => {
           </IntroStoryNormalPhoto>
         </IntroStoryUnitColumnMain>
         <IntroStoryUnitColumnSub>
-          <MessageTypography>
+          <IntroStoryIllustration1>
+            <IntersectionFadeIn slideIn fillLayout wait={500}>
+              <Picture relativePath="illustrations/intro/story_1.png" />
+            </IntersectionFadeIn>
+          </IntroStoryIllustration1>
+          <MessageTypography isShort>
             東京の一大ターミナル、新宿駅から一〇分。
             <br />
             昔なつかしい商店街、古着屋、カフェ。演劇にアート、阿波踊り……。あらゆる文化が混ざり合い、老若男女が集う街、高円寺。
@@ -142,20 +147,20 @@ export const IntroModule: React.FC = () => {
               関東大震災後、東京の中心部からやってきたファミリー層や高齢者世帯によって、新興住宅街がつくられたこのエリア。戦後になると、作家やアーティストをはじめ若者も多く移り住むようになり、多種多様な人びとが、ときに「中央線文化」とも呼ばれる独自のカルチャーを形作ってきました。
             </MessageTypography>
           </IntersectionFadeIn>
-          <IntroStoryIllustration1>
-            <IntersectionFadeIn fillLayout wait={500}>
-              <Picture relativePath="illustrations/intro/story_1.png" />
+          <IntroStoryIllustration2>
+            <IntersectionFadeIn slideIn fillLayout wait={500}>
+              <Picture relativePath="illustrations/intro/story_2.png" />
             </IntersectionFadeIn>
-          </IntroStoryIllustration1>
+          </IntroStoryIllustration2>
         </MessageInGrid>
       </GridOuter>
       <IntroStoryUnitNormal>
         <IntroStoryUnitColumnSub>
-          <IntroStoryIllustration2>
-            <IntersectionFadeIn fillLayout wait={500}>
-              <Picture relativePath="illustrations/intro/story_2.png" />
+          <IntroStoryIllustration3>
+            <IntersectionFadeIn slideIn fillLayout wait={500}>
+              <Picture relativePath="illustrations/intro/story_3.png" />
             </IntersectionFadeIn>
-          </IntroStoryIllustration2>
+          </IntroStoryIllustration3>
           <MessageTypography>
             車の通りも少なく、子どもや年配の方も安心して過ごせる。交通の便が良く、働く人にもやさしい。さまざまな人が暮らす、まさに「混沌」ということばで形容するにふさわしい場所です。
           </MessageTypography>
@@ -185,11 +190,11 @@ export const IntroModule: React.FC = () => {
           </IntersectionFadeIn>
         </IntroStoryUnitColumnMain>
         <IntroStoryUnitColumnSub>
-          <IntroStoryIllustration3>
-            <IntersectionFadeIn fillLayout wait={500}>
-              <Picture relativePath="illustrations/intro/story_3.png" />
+          <IntroStoryIllustration4>
+            <IntersectionFadeIn slideIn fillLayout wait={500}>
+              <Picture relativePath="illustrations/intro/story_4.png" />
             </IntersectionFadeIn>
-          </IntroStoryIllustration3>
+          </IntroStoryIllustration4>
           <MessageTypography>
             小杉湯は昭和八年（一九三三年）にこの街で生まれました。高円寺らしい「ごちゃ混ぜ感」をぎゅっと凝縮したような、街の銭湯。創業当時の建物を守りつつ、時代に合わせて中身を変え続け、高円寺の人々とともにあり続けてきました。
           </MessageTypography>
@@ -226,13 +231,20 @@ export const IntroModule: React.FC = () => {
   );
 };
 
-const MessageTypography: React.FC = ({ children }) => {
+type MessageTypographyProps = {
+  isShort?: boolean;
+};
+
+const MessageTypography: React.FC<MessageTypographyProps> = ({ isShort, children }) => {
   const ref = useRef<HTMLSpanElement>(null);
   const width = ref.current?.clientWidth;
 
   return (
     <IntersectionFadeIn>
-      <MessageTypographyLayout style={isSafari() && width ? { width: `${width}px` } : {}}>
+      <MessageTypographyLayout
+        isShort={isShort}
+        style={isSafari() && width ? { width: `${width}px` } : {}}
+      >
         <MessageTypographyStyle ref={ref}>{children}</MessageTypographyStyle>
       </MessageTypographyLayout>
     </IntersectionFadeIn>
@@ -249,11 +261,11 @@ const MessageTypographyStyle = styled.span`
   `}
 `;
 
-const MessageTypographyLayout = styled.p`
+const MessageTypographyLayout = styled.p<Pick<MessageTypographyProps, 'isShort'>>`
   height: 400px;
 
-  ${media.lessThan(ScreenType.MEDIUM)`
-    height: 380px;
+  ${media.lessThan<Pick<MessageTypographyProps, 'isShort'>>(ScreenType.MEDIUM)`
+    height: ${({ isShort }) => (isShort ? 340 : 380)}px;
   `}
 `;
 
@@ -273,6 +285,10 @@ const IntroStoryUnitColumnSub = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-end;
+
+  ${media.lessThan(ScreenType.MEDIUM)`
+    justify-content: flex-start;
+  `}
 `;
 
 const IntroStoryUnitColumnMain = styled.div`
@@ -300,33 +316,56 @@ const IntroStoryNormalPhoto = styled.div`
 
 const IntroStoryIllustration = styled.div`
   position: absolute;
-  width: 100px;
+  width: 240px;
   z-index: 1;
 
   ${media.lessThan(ScreenType.MEDIUM)`
-    width: 72px;
+    width: 160px;
   `}
 `;
 
 const IntroStoryIllustration1 = styled(IntroStoryIllustration)`
-  top: calc(100% + ${Spacing.LARGE}px);
-  right: 0;
+  top: 12%;
+  left: -${Spacing.XX_LARGE}px;
 
   ${media.lessThan(ScreenType.MEDIUM)`
-    right: 40%;
+    top: auto;
+    bottom: -52px;
+    left: -12px;
   `}
 `;
 
 const IntroStoryIllustration2 = styled(IntroStoryIllustration)`
-  top: 20%;
-  right: -${Spacing.LARGE}px;
+  top: calc(100% + 16px);
+  right: 0;
+
+  ${media.lessThan(ScreenType.MEDIUM)`
+    width: 180px;
+    top: auto;
+    bottom: -32px;
+    right: auto;
+    left: calc(100% - 40px);
+  `}
 `;
 
 const IntroStoryIllustration3 = styled(IntroStoryIllustration)`
-  top: 30%;
-  left: ${Spacing.LARGE}px;
+  top: 20%;
+  right: -32px;
+
   ${media.lessThan(ScreenType.MEDIUM)`
-    left: -${Spacing.XX_LARGE}px;
+    top: auto;
+    bottom: -120px;
+    right: -32px;
+  `}
+`;
+
+const IntroStoryIllustration4 = styled(IntroStoryIllustration)`
+  top: 14%;
+  left: -${Spacing.LARGE}px;
+  ${media.lessThan(ScreenType.MEDIUM)`
+    top: auto;
+    bottom: -108px;
+    left: -24px;
   `}
 `;
 
